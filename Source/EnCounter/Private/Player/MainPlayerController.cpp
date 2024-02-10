2bdae9ffe0d5c08ae50ca41da77cbb072fa5a939
Mondea Sorin <moneas2001@gmail.com>
2024-02-10 21:24:01 +0200

    Enhanced Player Controller intialized

---
+++ Source/EnCounter/Private/Player/MainPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright Mondea Sorin-Gabriel
+
+
+#include "MainPlayerController.h"
+
---
+++ Source/EnCounter/Public/Player/MainPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Mondea Sorin-Gabriel
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENCOUNTER_API AMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};

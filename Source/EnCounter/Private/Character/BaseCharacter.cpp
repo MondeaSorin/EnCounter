973b1c2e317fbbd1ea11ff004f8eb029d6b3061f
Mondea Sorin <moneas2001@gmail.com>
2024-02-10 18:47:36 +0200

    Modules created

---
+++ Source/EnCounter/Private/Character/BaseCharacter.cpp
@@ -0,0 +1,17 @@
+// Copyright Mondea Sorin-Gabriel
+
+
+#include "BaseCharacter.h"
+
+ABaseCharacter::ABaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/EnCounter/Public/Character/BaseCharacter.h
@@ -0,0 +1,19 @@
+// Copyright Mondea Sorin-Gabriel
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS(Abstract)
+class ENCOUNTER_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABaseCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+};

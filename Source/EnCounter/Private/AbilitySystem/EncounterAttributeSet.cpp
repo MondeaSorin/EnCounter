7ffa3fdeb6af3b125f0d4c175efe1be239a1477c
Mondea Sorin <moneas2001@gmail.com>
2024-02-16 23:56:04 +0200

    Health and Mana attributes added

--- Source/EnCounter/Private/AbilitySystem/EncounterAttributeSet.cpp
+++ Source/EnCounter/Private/AbilitySystem/EncounterAttributeSet.cpp
@@ -3,3 +3,40 @@
 
 #include "AbilitySystem/EncounterAttributeSet.h"
 
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UEncounterAttributeSet::UEncounterAttributeSet()
+{
+	
+}
+
+void UEncounterAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UEncounterAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEncounterAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEncounterAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UEncounterAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UEncounterAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEncounterAttributeSet, Health, OldHealth);
+}
+
+void UEncounterAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEncounterAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UEncounterAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEncounterAttributeSet, Mana, OldMana);
+}
+
+void UEncounterAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UEncounterAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/EnCounter/Public/AbilitySystem/EncounterAttributeSet.h
+++ Source/EnCounter/Public/AbilitySystem/EncounterAttributeSet.h
@@ -13,5 +13,34 @@ UCLASS()
 class ENCOUNTER_API UEncounterAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UEncounterAttributeSet();
+	
+	virtual void GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
